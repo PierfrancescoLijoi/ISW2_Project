585463e763cb67cc5c28116942ba203e706c5912
PierfrancescoLijoi <pierfrancesco.lijoi@libero.it>
2024-04-07 22:53:04 +0200

    aggiunta prelievo di tutti i ticket bug risolti. Inziata l'identificazione dell'OP,FV e IV. da concludere con prototion

---
+++ src/main/java/org/isw2_project/commonFunctions/TicketOperations.java
@@ -0,0 +1,19 @@
+package org.isw2_project.commonFunctions;
+
+import org.isw2_project.models.Release;
+import org.isw2_project.models.Ticket;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class TicketOperations {
+    private TicketOperations(){}
+    public static List<Ticket> fixTicketList(List<Ticket> ticketsList, List<Release> releasesList, String projName){
+        List<Ticket> ticketsForProportionList = new ArrayList<>();
+        List<Ticket> finalTicketsList = new ArrayList<>();
+        float proportion;
+
+        return finalTicketsList;
+    }
+}
